ad45a88c5a5ec2dac488c8cb629c89ac529302e7
sanjay311999 <sanjay311999@gmail.com>
2019-12-23 19:17:43 +0530

    clean directory

---
+++ Database_dump/nfdb_vehicle1ai.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE  IF NOT EXISTS `nfdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `nfdb`;
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: nfdb
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `vehicle1ai`
+--
+
+DROP TABLE IF EXISTS `vehicle1ai`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vehicle1ai` (
+  `vehicle_id` varchar(15) NOT NULL,
+  `area_id` varchar(10) NOT NULL,
+  PRIMARY KEY (`vehicle_id`),
+  KEY `area_id` (`area_id`),
+  CONSTRAINT `vehicle1ai_ibfk_1` FOREIGN KEY (`area_id`) REFERENCES `area` (`area_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle1ai`
+--
+
+LOCK TABLES `vehicle1ai` WRITE;
+/*!40000 ALTER TABLE `vehicle1ai` DISABLE KEYS */;
+INSERT INTO `vehicle1ai` VALUES ('TN38I3347','a201'),('TN39H6509','a202'),('TN38A6947','a203'),('TN38G6447','a203'),('TN01F1237','a204'),('TN33B2347','a205'),('TN38E5347','a205'),('TN39D1147','a206'),('TN60C9047','a207');
+/*!40000 ALTER TABLE `vehicle1ai` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-04 21:47:15
--- newapp/backend/json-table.js
+++
@@ -1,4 +0,0 @@
-const Json2Html = require('json2htable');
-let data = [{ ab: "ab" }]
-let table = Json2Html(data);
-console.log(table);
